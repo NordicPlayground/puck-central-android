839be1d6673419368d4577664ac8a09b44fe7932
aleksanb <aleksanderburkow@gmail.com>
2014-06-20 15:12:22 +0200

    Add Location Puck actuator dialog && long press to delete puck

--- Evere/app/src/main/java/no/nordicsemi/activities/MainActivity.java
+++ Evere/app/src/main/java/no/nordicsemi/activities/MainActivity.java
@@ -8,7 +8,11 @@ import android.os.RemoteException;
 import android.telephony.gsm.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.radiusnetworks.ibeacon.IBeacon;
@@ -19,6 +23,7 @@ import com.radiusnetworks.ibeacon.Region;
 
 import org.droidparts.activity.Activity;
 import org.droidparts.annotation.inject.InjectView;
+import org.droidparts.model.Entity;
 import org.json.JSONException;
 
 import java.util.Collection;
@@ -53,6 +58,31 @@ public class MainActivity extends Activity implements IBeaconConsumer {
                 .select());
         lvLocationPucks.setAdapter(adapter);
 
+        lvLocationPucks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Entity selected = adapter.read(position);
+                showLocationPuckActuatorsDialog((LocationPuck) selected);
+            }
+        });
+
+        lvLocationPucks.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(getString(R.string.puck_remove))
+                        .setPositiveButton(getString(R.string.delete), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                adapter.delete(position);
+                            }
+                        })
+                        .setNegativeButton(getString(R.string.abort), null);
+                builder.create().show();
+                return true;
+            }
+        });
+
         // Called for debug purposes. Should be moved to iBeacon location entered callback.
         locationPuckDiscovered();
     }
@@ -73,6 +103,29 @@ public class MainActivity extends Activity implements IBeaconConsumer {
                 .setNegativeButton(getString(R.string.reject), null);
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    public void showLocationPuckActuatorsDialog(LocationPuck locationPuck) {
+        String[] names = getResources().getStringArray(R.array.locationPuckNames);
+        String[] puckActuators = getResources().getStringArray(
+                locationPuck.getName().equals(names[0]) ?
+                        R.array.officeActuators : R.array.kitchenActuators);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        View v = getLayoutInflater().inflate(R.layout.dialog_location_puck_actuations, null);
+
+        TextView tvPuckActuatorTitle = (TextView) v.findViewById(R.id.tvPuckActuatorTitle);
+        tvPuckActuatorTitle.setText("Actuators for " + locationPuck.getName());
+
+        ListView actuatorsList = (ListView) v.findViewById(R.id.lvPuckActuators);
+        ArrayAdapter<String> puckActuatorsAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, puckActuators);
+        actuatorsList.setAdapter(puckActuatorsAdapter);
+
+        builder.setView(v);
+
+        builder.create().show();
     }
 
     @Override
