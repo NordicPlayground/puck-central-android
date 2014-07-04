ff0eb6623de91f81e67f5293dead280e2eab1ea1
aleksanb <aleksanderburkow@gmail.com>
2014-07-04 13:59:15 +0200

    Fix sign bug in NumberUtils
    Large values processed by stringNumberToByteArray would be incorrectly
    processed due to sign bit issues, causing problems with bluetooth
    connections. This commit fixes this issue.
    See http://stackoverflow.com/q/4407779 for more information.

--- Evere/app/src/main/java/no/nordicsemi/utils/NumberUtils.java
+++ Evere/app/src/main/java/no/nordicsemi/utils/NumberUtils.java
@@ -7,7 +7,9 @@ public class NumberUtils {
     public static byte[] stringNumberToByteArray(String number, int radix, int size) {
         byte[] array = new BigInteger(number, radix).toByteArray();
         byte[] sizedArray = new byte[size];
-        System.arraycopy(array, 0, sizedArray, 0, Math.min(size, array.length));
+        int signOffset = array[0] == 0 ? 1 : 0; //http://stackoverflow.com/q/4407779
+        System.arraycopy(array, signOffset, sizedArray, 0, Math.min(size, array.length));
+
         return sizedArray;
     }
 }
